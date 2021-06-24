c477e020c8d4b4ad954b00d2e062c1227bf04555
Edi Wang <Edi.Wang@outlook.com>
2021-06-24 13:40:03 +0800

    clean up

--- RMBCap/Program.cs
+++ RMBCap/Program.cs
@@ -1,8 +1,5 @@
-using System;
-using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace RMBCap
 {
@@ -12,8 +9,6 @@ namespace RMBCap
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
-
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
         }
