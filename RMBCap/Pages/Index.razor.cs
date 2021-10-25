2514666925dd346c6ea8f66eaf57282285003639
Edi Wang <Edi.Wang@outlook.com>
2021-10-25 13:04:00 +0800

    upgrade to .NET 6 RC2

--- RMBCap/Pages/Index.razor.cs
+++ RMBCap/Pages/Index.razor.cs
@@ -1,99 +1,94 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Components;
+﻿using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
 
-namespace RMBCap.Pages
+namespace RMBCap.Pages;
+
+public partial class Index
 {
-    public partial class Index
+    [Inject]
+    public IJSRuntime JavaScriptRuntime { get; set; }
+
+    private string _inputAmount;
+
+    public string InputAmount
     {
-        [Inject]
-        public IJSRuntime JavaScriptRuntime { get; set; }
-
-        private string _inputAmount;
-
-        public string InputAmount
+        get => _inputAmount;
+        set
         {
-            get => _inputAmount;
-            set
-            {
-                _inputAmount = value;
+            _inputAmount = value;
 
-                if (value.Length <= 11)
+            if (value.Length <= 11)
+            {
+                if (!string.IsNullOrEmpty(value) && value.Contains("."))
                 {
-                    if (!string.IsNullOrEmpty(value) && value.Contains("."))
+                    var parts = value.Split('.');
+                    var decPart = parts[1];
+                    if (decPart.Length <= 2)
                     {
-                        var parts = value.Split('.');
-                        var decPart = parts[1];
-                        if (decPart.Length <= 2)
+                        if (value == ".")
                         {
-                            if (value == ".")
-                            {
-                                value = "0.";
-                                _inputAmount = value;
-                            }
-                            else
-                            {
-                                _inputAmount = value;
-                            }
+                            value = "0.";
+                            _inputAmount = value;
                         }
                         else
                         {
-                            return;
+                            _inputAmount = value;
                         }
-                    }
-
-                    // e.g. 01234... format to 1234
-                    if (value.StartsWith("0") && !value.Contains("."))
-                    {
-                        value = int.Parse(value).ToString();
-                        _inputAmount = value;
                     }
                     else
                     {
-                        _inputAmount = value;
+                        return;
                     }
-
-                    Result = string.IsNullOrWhiteSpace(_inputAmount) ?
-                        string.Empty :
-                        RMBConverter.RMBToCap(InputAmount);
                 }
+
+                // e.g. 01234... format to 1234
+                if (value.StartsWith("0") && !value.Contains("."))
+                {
+                    value = int.Parse(value).ToString();
+                    _inputAmount = value;
+                }
+                else
+                {
+                    _inputAmount = value;
+                }
+
+                Result = string.IsNullOrWhiteSpace(_inputAmount) ?
+                    string.Empty :
+                    RMBConverter.RMBToCap(InputAmount);
             }
         }
+    }
 
-        public string Result { get; set; }
+    public string Result { get; set; }
 
-        private async Task CopyResult()
+    private async Task CopyResult()
+    {
+        if (!string.IsNullOrWhiteSpace(Result))
         {
-            if (!string.IsNullOrWhiteSpace(Result))
-            {
-                await JavaScriptRuntime.InvokeVoidAsync("clipboardCopy.copyText", Result);
-            }
+            await JavaScriptRuntime.InvokeVoidAsync("clipboardCopy.copyText", Result);
         }
+    }
 
-        private async Task ReadAloud()
+    private async Task ReadAloud()
+    {
+        if (!string.IsNullOrWhiteSpace(Result))
         {
-            if (!string.IsNullOrWhiteSpace(Result))
-            {
-                await JavaScriptRuntime.InvokeVoidAsync("readAloud.readText", Result);
-            }
+            await JavaScriptRuntime.InvokeVoidAsync("readAloud.readText", Result);
         }
+    }
 
-        private void Clear()
-        {
-            InputAmount = string.Empty;
-        }
+    private void Clear()
+    {
+        InputAmount = string.Empty;
+    }
 
-        private void KeyPadClicked(string value)
+    private void KeyPadClicked(string value)
+    {
+        InputAmount += value switch
         {
-            InputAmount += value switch
-            {
-                "0" when InputAmount != "0" => 0,
-                "." when !InputAmount.Contains(".") => ".",
-                _ => value
-            };
-        }
+            "0" when InputAmount != "0" => 0,
+            "." when !InputAmount.Contains(".") => ".",
+            _ => value
+        };
     }
 }
--- RMBCap/Program.cs
+++ RMBCap/Program.cs
@@ -1,16 +1,14 @@
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-namespace RMBCap
-{
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
+namespace RMBCap;
 
-            await builder.Build().RunAsync();
-        }
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        builder.RootComponents.Add<App>("#app");
+
+        await builder.Build().RunAsync();
     }
 }
